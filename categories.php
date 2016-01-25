d6a4206700be3b67cc83aa3823ac35a7598bcd70
Alex Krizhanovsky <alex@enot.vc>
2016-01-25 16:57:18 +0300

    v1

---
+++ categories.php
@@ -0,0 +1,10 @@
+<?php
+
+    return [
+        'astronomy', 'geology', 'gyroscope',
+        'literature', 'marketing', 'mathematics',
+        'music', 'polit', 'agrobiologia',
+        'law', 'psychology', 'geography',
+        'physics', 'philosophy', 'chemistry',
+        'estetica'
+    ];
---
+++ config.example.php
@@ -0,0 +1,17 @@
+<?php
+
+
+    return [
+        'DB_HOST'               => 'dbhost',
+        'DB_NAME'               => 'dbbase',
+        'DB_BUSER'              => 'dbuser',
+        'DB_PASSWORD'           => 'dbuserpassword',
+        'DB_TABLE'              => 'dbtable',
+        'DB_CONNECTION_CHARSET' => 'UTF8',
+        'base_url'              => 'http://referats.yandex.ru/referats/write/',
+        'FIELDS_MAPPER'    => [
+            'title'     => 'title', // field title in table
+            'text'      => 'text',
+            'preview'   => 'preview'
+        ]
+    ];
